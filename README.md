5c441d254d5314bcf347fd7ee4cb17bbc066f100
Michael Fleps <f.michi84.989@gmail.com>
2023-08-23 23:31:50 +0200

    2. SplashActivity definiert
    - layout und progressbar
    - Farben definiert
    - auf Stringresourcen verwiesen

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ### Hallo, hier programmiere ich eine Shop App, ich nutze viewBinding, dataBinding und werde nach der MVVM Architektur Bauen...
 
 ### 1. Packages und benötigten Dateine nach Model-View-ViewModel erstellen
-### 2. viewBinding, dataBinding und die Navigation aktivieren
-### 3. 
+### 2. viewBinding und die Navigation aktivieren
+### 3. SplashActivity definieren und im AndroidManifest den intent-filter verschieben 
 ### 4.
 ### 5.
 ### 6.
--- app/src/main/java/com/example/meinshop/ui/SplashActivity.kt
+++ app/src/main/java/com/example/meinshop/ui/SplashActivity.kt
@@ -1,12 +1,41 @@
 package com.example.meinshop.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.meinshop.R
+import android.os.CountDownTimer
+import com.example.meinshop.MainActivity
+import com.example.meinshop.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    // Deklariert das Binding-Objekt für die Aktivität
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        // Initialisiert das Binding-Objekt
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Setzt das Maximum der ProgressBar auf 100
+        binding.splashPB.max = 100
+
+        // Startet den eingebauten Countdown der Timer Klasse und läuft nach 3 Sekunden ab
+        object: CountDownTimer(3500,30) {
+            override fun onTick(millisUntilFinished: Long) {
+                // "onTick" die Methode wird jede 30 millisekunden aufgerufen und aktualisiert den Fortschritt der Progressbar
+                val progress = ((3500 - millisUntilFinished) / 30).toInt()
+                binding.splashPB.progress = progress
+            }
+
+            // Methode wird aufgerufen wenn der Countdown abgelaufen ist, die MainActivity startet hier und die SplashActivity wird beendet
+            override fun onFinish() {
+                //  die Progressbar ist voll wenn der Timer abgelaufen ist
+                binding.splashPB.progress = 100
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            }
+        }.start()   // startet den Timer
     }
 }
